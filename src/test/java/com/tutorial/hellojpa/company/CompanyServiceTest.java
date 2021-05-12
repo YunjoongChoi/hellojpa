e6db2f978c4cf3778a3079ca8ccfcb6f9f574625
YunjoongChoi <choimory@illunex.com>
2021-05-12 19:07:57 +0900

    - 테스트 코드 작성

--- src/test/java/com/tutorial/hellojpa/company/CompanyServiceTest.java
+++ src/test/java/com/tutorial/hellojpa/company/CompanyServiceTest.java
@@ -1,13 +1,44 @@
 package com.tutorial.hellojpa.company;
 
+import com.tutorial.hellojpa.company.dto.CompanyDto;
+import com.tutorial.hellojpa.company.repository.CompanyRepository;
+import com.tutorial.hellojpa.company.service.CompanyService;
+import com.tutorial.hellojpa.company.service.CompanyServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class CompanyServiceTest {
+    @Mock
+    private CompanyRepository companyRepository;
+    @InjectMocks
+    private CompanyServiceImpl companyService;
 
     @Test
-    public void 목록_조회_테스트(){
+    public void 전체_조회_테스트(){
+        /*given*/
 
+        /*when*/
+        List<CompanyDto> companyDtoList = companyService.getList(CompanyDto.builder().build(), null, null);
+
+        /*then*/
+        assertThat(companyDtoList.size() == 11);
+    }
+
+    @Test
+    public void 상세_조회_테스트(){
+        /*given*/
+
+        /*when*/
+        CompanyDto companyDto = companyService.getCompany(10L);
+
+        /*then*/
+        assertThat("기업10".equals(companyDto.getCompanyName()));
     }
 }
