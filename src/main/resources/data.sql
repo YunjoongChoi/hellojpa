30a8a04d605e6208919f3da13f6d999e753d7659
YunjoongChoi <choimory@illunex.com>
2021-05-12 16:02:34 +0900

    - 더미데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,14 @@
-INSERT INTO COMPANY (ADDRESS, COMPANY_NAME, COMPANY_TYPE, DELETE_AT, DESCRIPT, FOUNDATE_DATE_TIME, HOMEPAGE, REGIST_DATE_TIME, TEL, UPDATE_DATE_TIME, ZIPCODE) VALUES ('주소', '기업명', 'IT', 1, '설명', null, 'https://naver.com', now(), '02-123-4567', null, 12345)
+INSERT INTO COMPANY (ADDRESS
+                    , COMPANY_NAME
+                    , COMPANY_TYPE
+                    , DELETE_AT
+                    , DESCRIPT
+                    , FOUNDATE_DATE_TIME
+                    , HOMEPAGE
+                    , REGIST_DATE_TIME
+                    , TEL
+                    , UPDATE_DATE_TIME
+                    , ZIPCODE)
+VALUES ('주소1', '기업1', 'IT', 1, '설명1', '2015-01-01 00:00:00', 'https://www.naver.com', now(), '02-123-4567', null, 12345)
+        , ('주소2', '기업2', '금융', 1, '설명2', '2015-12-12', 'https://www.naver.com', now(), '02-123-4567', null, 12345)
+        , ('주소3', '기업3', '반도체', 1, '설명3', '2015-12-12 12:34:56', 'https://www.naver.com', now(), '02-123-4567', null, 12345);
