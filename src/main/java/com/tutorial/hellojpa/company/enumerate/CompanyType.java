0b1d0fee554980755849a69f2047bd36d11cb31a
YunjoongChoi <choimory@illunex.com>
2021-05-12 16:15:24 +0900

    - CompanyType Enum 변경

--- src/main/java/com/tutorial/hellojpa/company/enumerate/CompanyType.java
+++ src/main/java/com/tutorial/hellojpa/company/enumerate/CompanyType.java
@@ -1,5 +1,16 @@
 package com.tutorial.hellojpa.company.enumerate;
 
+import lombok.Getter;
+
+@Getter
 public enum CompanyType {
-    IT
+    IT("IT")
+    , FINANCE("금융")
+    , SEMICONDUCTOR("반도체");
+
+    private String value;
+
+    private CompanyType(String value) {
+        this.value = value;
+    }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,5 +10,5 @@ INSERT INTO COMPANY (ADDRESS
                     , UPDATE_DATE_TIME
                     , ZIPCODE)
 VALUES ('주소1', '기업1', 'IT', 1, '설명1', '2015-01-01 00:00:00', 'https://www.naver.com', now(), '02-123-4567', null, 12345)
-        , ('주소2', '기업2', '금융', 1, '설명2', '2015-12-12', 'https://www.naver.com', now(), '02-123-4567', null, 12345)
-        , ('주소3', '기업3', '반도체', 1, '설명3', '2015-12-12 12:34:56', 'https://www.naver.com', now(), '02-123-4567', null, 12345);
+        , ('주소2', '기업2', 'FINANCE', 1, '설명2', '2015-12-12', 'https://www.naver.com', now(), '02-123-4567', null, 12345)
+        , ('주소3', '기업3', 'SEMICONDUCTOR', 1, '설명3', '2015-12-12 12:34:56', 'https://www.naver.com', now(), '02-123-4567', null, 12345);
