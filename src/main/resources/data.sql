85b0270a80f5e7ddb51e897b0b5801b978d34efa
YunjoongChoi <choimory@illunex.com>
2021-05-12 17:01:23 +0900

    - 더미 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,5 +10,13 @@ INSERT INTO COMPANY (ADDRESS
                     , UPDATE_DATE_TIME
                     , ZIPCODE)
 VALUES ('주소1', '기업1', 'IT', 1, '설명1', '2015-01-01 00:00:00', 'https://www.naver.com', now(), '02-123-4567', null, 12345)
-        , ('주소2', '기업2', 'FINANCE', 1, '설명2', '2015-12-12', 'https://www.naver.com', now(), '02-123-4567', null, 12345)
-        , ('주소3', '기업3', 'SEMICONDUCTOR', 1, '설명3', '2015-12-12 12:34:56', 'https://www.naver.com', now(), '02-123-4567', null, 12345);
+     , ('주소2', '기업2', 'FINANCE', 1, '설명2', '2015-12-12', 'https://www.naver.com', now(), '02-123-4567', null, 12345)
+     , ('주소3', '기업3', 'SEMICONDUCTOR', 1, '설명4', '2015-12-12 12:34:56', 'https://www.naver.com', now(), '02-123-4567', null, 12345)
+     , ('주소4', '기업4', 'FINANCE', 1, '설명2', '2015-12-12', 'https://www.naver.com', now(), '02-123-4567', null, 12345)
+     , ('주소5', '기업5', 'SEMICONDUCTOR', 1, '설명5', '2015-12-12 12:34:56', 'https://www.naver.com', now(), '02-123-4567', null, 12345)
+     , ('주소6', '기업6', 'FINANCE', 1, '설명6', '2015-12-12', 'https://www.naver.com', now(), '02-123-4567', null, 12345)
+     , ('주소7', '기업7', 'SEMICONDUCTOR', 1, '설명7', '2015-12-12 12:34:56', 'https://www.naver.com', now(), '02-123-4567', null, 12345)
+     , ('주소8', '기업8', 'FINANCE', 1, '설명8', '2015-12-12', 'https://www.naver.com', now(), '02-123-4567', null, 12345)
+     , ('주소9', '기업9', 'SEMICONDUCTOR', 1, '설명9', '2015-12-12 12:34:56', 'https://www.naver.com', now(), '02-123-4567', null, 12345)
+     , ('주소10', '기업10', 'FINANCE', 1, '설명10', '2015-12-12', 'https://www.naver.com', now(), '02-123-4567', null, 12345)
+     , ('주소11', '기업11', 'SEMICONDUCTOR', 1, '설명11', '2015-12-12 12:34:56', 'https://www.naver.com', now(), '02-123-4567', null, 12345);
