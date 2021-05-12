5bccc5948fcbc71b1ea680c75efd306a2e11e7de
YunjoongChoi <choimory@illunex.com>
2021-05-12 15:36:41 +0900

    - 더미데이터 추가

---
+++ src/main/resources/dummy-data.sql
@@ -0,0 +1,14 @@
+INSERT INTO COMPANY(
+    COMPANY_IDX
+    , ADDRESS
+    , COMPANY_NAME
+    , COMPANY_TYPE
+    , DELETE_AT
+    , DESCRIPT
+    , FOUNDATE_DATE_TIME
+    , HOMEPAGE
+    , REGIST_DATE_TIME
+    , TEL
+    , UPDATE_DATE_TIME
+    , ZIPCODE)
+VALUES(1, '서울시 마포구', '일루넥스', 'IT', 0, '설명', '2015-01-01 00:00:00', 'https://naver.com', now(), '02-123-4567', null, 01234)
