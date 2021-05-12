5830493445490b98a8bac5dfde82c78126fd4add
YunjoongChoi <choimory@illunex.com>
2021-05-12 19:08:25 +0900

    - 테스트코드 작성 준비

--- src/test/java/com/tutorial/hellojpa/employee/EmployeeServiceTest.java
+++ src/test/java/com/tutorial/hellojpa/employee/EmployeeServiceTest.java
@@ -1,4 +1,16 @@
 package com.tutorial.hellojpa.employee;
 
+import com.tutorial.hellojpa.employee.repository.EmployeeRepository;
+import com.tutorial.hellojpa.employee.service.EmployeeService;
+import com.tutorial.hellojpa.employee.service.EmployeeServiceImpl;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class EmployeeServiceTest {
+    @Mock
+    EmployeeRepository employeeRepository;
+    @InjectMocks
+    EmployeeServiceImpl employeeService;
 }
